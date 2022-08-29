dc823b7d00ae5fc0aa00d7736932b3a2abb5b96a
SubasriSampath <subasrivasanthan@gmail.com>
2022-08-29 15:27:09 +0530

    verify new class

---
+++ src/test/java/com/simple/NewClass.java
@@ -0,0 +1,10 @@
+package com.simple;
+
+public class NewClass {
+	private void Method1() {
+		System.out.println("Method 1");
+	}
+	private void Method2() {
+		System.out.println("Method 2");
+	}
+}
